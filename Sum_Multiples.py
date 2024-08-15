fab5b180f922d19e3c497306dcb75b92cdc45157
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-08-15 21:26:36 +0100

    feat: Implementing a function to calculate sum of numbers divisible by 3, 5, or 7 within a given range.

---
+++ Sum_Multiples.py
@@ -0,0 +1,13 @@
+def sum_of_divisibles(n):
+    total_sum = 0
+
+    for i in range(1, n + 1):
+
+        if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+            total_sum += i
+
+    return total_sum
+
+
+n = 15
+print(sum_of_divisibles(n))
