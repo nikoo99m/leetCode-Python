298cb46bc75f302ee891769c4746dc8fd54047de
nikoo99m <nmogha02@student.bbk.ac.uk>
2024-10-13 21:51:37 +0100

    feat: Implementing a method to reverse a string in an efficient way.

--- find_disappeared_numbers.py
+++ find_disappeared_numbers.py
@@ -9,7 +9,7 @@ def findDisappearedNumbers(nums):
 
 
 if __name__ == "__main__":
-    nums = [4, 3, 2, 7, 8, 2, 3, 1]
+    nums = [4, 3, 2, 7, 8, 2, 3, 1, 9]
     missing_numbers = findDisappearedNumbers(nums)
 
     print("Missing numbers:", missing_numbers)
---
+++ reverse_string.py
@@ -0,0 +1,9 @@
+def reverse_string(s):
+    return s[::-1]
+
+
+if __name__ == "__main__":
+    input1 = "Nikoo"
+    reversed1 = reverse_string(input1)
+    print("Original:", input1)
+    print("Reversed:", reversed1)
